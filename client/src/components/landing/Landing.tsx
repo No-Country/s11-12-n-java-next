dfdae0851b7cb9b2222f621400881edb836c40d9
fernandobouchet <fernandobouchet@gmail.com>
2023-10-19 00:26:35 -0300

    feat: update 'Landing' and 'Sliders'

--- client/src/components/landing/Landing.tsx
+++ client/src/components/landing/Landing.tsx
@@ -2,7 +2,7 @@ import Slider from "@/components/landing/slider/Slider";
 
 const Landing = () => {
   return (
-    <div className="flex w-full h-screen flex-col p-3">
+    <div className="flex w-full h-screen flex-col">
       <Slider />
     </div>
   );
--- client/src/components/landing/slider/SliderContent.tsx
+++ client/src/components/landing/slider/SliderContent.tsx
@@ -7,21 +7,27 @@ interface Props {
 
 const SliderContent = ({ currentSlider }: Props) => {
   const data = sliderLandingData;
-  
+
   return (
-    <div className="h-full flex flex-col items-center justify-evenly">
+    <>
       {data.map((slide, index) => (
         <div
           key={slide.id}
-          className={`h-full flex flex-col items-center justify-evenly ${
+          className={`h-full flex flex-col items-center gap-6 ${
             currentSlider !== index ? "hidden" : ""
           }`}
         >
           <Image src={slide.image.src} alt={slide.image.alt} />
-          <p className="text-center">{slide.text}</p>
+          <p
+            className={`text-center text-2xl px-4 ${
+              currentSlider === 0 ? "font-light" : ""
+            }`}
+          >
+            {slide.text}
+          </p>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 export default SliderContent;
