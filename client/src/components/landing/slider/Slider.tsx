e0047602735008fa4ad78c970862ea77beae9895
“agtelo” <alantelo1987@gmail.com>
2023-10-18 21:49:14 -0300

    fix: routing update

--- client/src/components/landing/slider/Slider.tsx
+++ client/src/components/landing/slider/Slider.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Button from "@/components/ui/button/Button";
-import { useRouter } from "next/navigation";
-import { sliderLandingData } from "@/utils/constants";
-import SliderContent from "./SliderContent";
+import { useState } from 'react';
+import Button from '@/components/ui/button/Button';
+import { useRouter } from 'next/navigation';
+import { sliderLandingData } from '@/utils/constants';
+import SliderContent from './SliderContent';
 
 const Slider = () => {
   const router = useRouter();
@@ -13,19 +13,19 @@ const Slider = () => {
 
   const handleSetCurrentSlider = (number: number) => {
     if (number >= sliderLandingData.length) {
-      router.push("/login");
+      router.push('/welcome');
     } else {
       setcurrentSlider(number);
     }
   };
 
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className='flex flex-col w-full h-full'>
       <SliderContent currentSlider={currentSlider} />
-      <div className="mt-auto mb-14 w-full">
+      <div className='mt-auto mb-14 w-full'>
         <Button
-          className="w-full"
-          size="xl"
+          className='w-full'
+          size='xl'
           onClick={() => handleSetCurrentSlider(currentSlider + 1)}
         >
           Siguiente
