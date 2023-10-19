04a421d7ed6fce994ad77cc2d26f639517aaa171
Ivan Mieres <mieres.iv@gmail.com>
2023-10-18 22:26:30 -0300

    fix: user dto

--- back/quizwiz/src/main/java/com/selecionado/quizwiz/dto/request/UserDTOReq.java
+++ back/quizwiz/src/main/java/com/selecionado/quizwiz/dto/request/UserDTOReq.java
@@ -1,18 +1,4 @@
 package com.selecionado.quizwiz.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDtoReq {
-
-    private Long id;
-    private String fullName;
-    private String email;
-    private String password;
-    private String confirmPassword;
-    private Boolean enabled;
 }
