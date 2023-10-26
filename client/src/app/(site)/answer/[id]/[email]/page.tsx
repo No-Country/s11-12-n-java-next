5e3a05e75319131ca7fddc19e026105259735732
fernandobouchet <fernandobouchet@gmail.com>
2023-10-26 00:01:41 -0300

    feat: add 'cards' for pending 'quiz' and 'user', update answer pages

--- client/src/app/(site)/answer/[id]/[email]/page.tsx
+++ client/src/app/(site)/answer/[id]/[email]/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function AnswerList() {
-  return <div>AnswerList</div>;
+export default function AnswerList({ params }: { params: { email: string } }) {
+  return (
+    <div className="text-teal-700 text-xl flex flex-col items-center">
+      <h2>No Country Cohorte s11-12 A</h2>
+      <h2 className="font-black">{params.email}</h2>
+    </div>
+  );
 }
