d807a2f61f70c992dc4003461c420d4cab54a7c9
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-11-01 19:40:30 -0300

    Style: add auto margin on the x axis

--- client/src/components/filterForms/FormLayout.tsx
+++ client/src/components/filterForms/FormLayout.tsx
@@ -1,6 +1,6 @@
 export default function FormLayout({ children }: { children: React.ReactNode}) {
   return (
-    <section className="flex flex-col gap-4 w-80 pb-8">
+    <section className="flex flex-col mx-auto gap-4 w-80 pb-8">
       {children}
     </section>
   )
