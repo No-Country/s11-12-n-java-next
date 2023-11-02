13c8ab6a8b8a82bdbdd1548b493a99660fcd32f4
fernandobouchet <fernandobouchet@gmail.com>
2023-11-01 23:58:37 -0300

    feat: style updates

--- client/src/hooks/useForms.ts
+++ client/src/hooks/useForms.ts
@@ -1,20 +1,21 @@
-import { getFormsService, createFormService } from "@/services/form.service"
+import { getFormsService, createFormService } from "@/services/form.service";
 
 export default function useForms() {
-  const handleGetForms = async (data: FormSchema) => {
-    const res = await getFormsService(data)
+  const handleGetForms = async (data: FormSchema, token: string) => {
+    const res = await getFormsService(data, token);
 
-    return res
-  }
+    return res;
+  };
 
-  const handleCreateForms = async (data: FormSchema) => {
-    const res = await createFormService(data)
-    if (!res.resolved) console.error("Error: ", res)
-    
-    return res
-  }
+  const handleCreateForms = async (data: FormSchema, token: string) => {
+    const res = await createFormService(data, token);
+    if (!res.resolved) console.error("Error: ", res);
+
+    return res;
+  };
 
   return {
-    handleGetForms, handleCreateForms
-  }
+    handleGetForms,
+    handleCreateForms,
+  };
 }
